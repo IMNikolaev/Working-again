86a28ae085c627f38294db51d31f5c6574403584
Igor Nikolaev <tamarisk60@yandex.ru>
2024-02-28 19:44:54 +0100

    First Comit

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,7 +1,12 @@
 package view;
 
+import model.Book;
+import model.Reader;
+import util.MyLinkedList;
+
 public class Main {
     public static void main(String[] args) {
-
+        MyLinkedList<Book> books = new MyLinkedList<>();
+        MyLinkedList<Reader> readers = new MyLinkedList<>();
     }
 }
